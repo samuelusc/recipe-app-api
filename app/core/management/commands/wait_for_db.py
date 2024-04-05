233a6c48a9852d1999d124274f9b9567c8fb2b8f
samuelusc <wangjial@usc.edu>
2024-04-05 14:06:16 -0700

    Module to test custom Django command for ensuring database availability before app start

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,44 @@
+"""
+Django command to wait for the database to be available 
+"""
+# import  necessary modules
+import time
+from psycopg2 import OperationalError as Psycopg20Error
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """
+    Django command to wait for database is available.
+    This Command is useful in the situations when containerized environments where the 
+    web application might start before the database is ready.
+    """
+
+    def handle(self, *args, **options):
+        """
+        The main entry point for the command.
+        It continuously checks if the database is available and waits
+        for it if it's not yet ready.
+        """
+
+        # Notify the user that the command is waiting for the database
+        self.stdout.write('Waiting for database...')
+        # Flag to track if the database is up
+        db_up = False
+
+        # Continue checking the database status until it's up
+        while db_up is False:
+            # Check database connectivity; will throw an error if not available
+            try:
+                self.check(databases=['default'])
+                db_up = True
+
+            # If an error is raised, the database is not available
+            # Notify the user and wait for 1 second before retrying
+            except (Psycopg20Error, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
+        # Notify the user the database is ready
+        self.stdout.write(self.style.SUCCESS('Database available!'))
---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,43 @@
+"""
+Module to test custom Django management commands.
+"""
+# Import necessary testing utilities and mock functionality
+from unittest.mock import patch
+from psycopg2 import OperationalError as Psycopg2Error
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+# Patching the Command.check method used in wait_for_db command
+
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """
+    Test suite for the custom Django management command wait_for_db
+    It simulates different database availability scenarios to ensure
+    the command handles them appropriately.
+    """
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database ready."""
+        patched_check.return_value = True
+
+        # Execute the command to be tested
+        call_command('wait_for_db')
+
+        # Ensure the mocked check method was called only once with expected arguments
+        patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationalError."""
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        # Check if the mocked check method was called 6 times in total
+        self.assertEqual(patched_check.call_count, 6)
+        # Ensure the final call was with the expected arguments
+        patched_check.assert_called_with(databases=['default'])
